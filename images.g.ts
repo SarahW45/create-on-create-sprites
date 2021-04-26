7a3c1fb16730790e3858b37fcba399b7f0694976
SarahW45 <82471934+SarahW45@users.noreply.github.com>
2021-04-26 15:38:42 -0400

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,37 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "I|vq$$e4T^_C-(b#$W~f":
+            case "butterfly":return img`
+. . . . . . . . . . . . . . . . 
+. . . f . . . . f . . . . . . . 
+. . . . f . . f . . . . . . . . 
+. 3 3 3 3 f f c 3 3 3 . . . . . 
+. c 3 3 c f f 3 3 3 c . . . . . 
+. 3 3 3 3 f f 3 c 3 3 . . . . . 
+. 3 c 3 3 f f 3 3 3 3 . . . . . 
+. 3 3 3 3 f f 3 3 c 3 . . . . . 
+. 3 3 c 3 f f c 3 3 3 . . . . . 
+. 3 3 3 3 f f 3 3 3 3 . . . . . 
+. . . . . f f . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
